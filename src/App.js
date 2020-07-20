97956619e22073482a6121e6061f9791901be3c8
unknown <kkuksov@gmail.com>
2020-07-20 22:30:34 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Menu, Dropdown } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css'
 
 function App() {
+
+    const menu = (
+        <Menu>
+            <Menu.Item key="1">1st item</Menu.Item>
+            <Menu.Item key="2">2nd item</Menu.Item>
+            <Menu.Item key="3">3rd item</Menu.Item>
+        </Menu>
+    );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <Button type="primary">primary</Button>
+        <Button>secondary</Button>
+        <Dropdown overlay={menu}>
+          <Button>
+            Actions <DownOutlined />
+          </Button>
+        </Dropdown>
+      </>
   );
 }
 
